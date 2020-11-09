d53d5ee835091512f6dcb9e845fc4f784648b42c
Dyna <dhhoyt2002@gmail.com>
2020-11-08 17:16:33 -0700

    Stuff
    Created movement

--- KinematicBody.gd
+++ KinematicBody.gd
@@ -1,11 +1,11 @@
 extends KinematicBody
 
-var walk_accel = 10000
-var max_walk_speed = 5
-var jump_speed = 1
-var gravity = -50
-var sensitivity = 0.2
-var friction = 1000
+export(float) var walk_accel = 10000
+export(float) var max_walk_speed = 5
+export(float) var jump_speed = 0
+export(float) var gravity = -50
+export(float) var sensitivity = 0.2
+export(float) var friction = 1000
 
 var velocity = Vector3()
 var looking = Vector3()
@@ -14,13 +14,13 @@ var looking_y
 var viewport
 
 func _ready():
-	viewport = get_node("/root/Spatial/KinematicBody/Camera")
+	viewport = get_node("Viewport")
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _input(event):
 	if event is InputEventMouseMotion:
-		looking.y += deg2rad(event.relative.x * sensitivity)
-		looking.x += deg2rad(event.relative.y * sensitivity)
+		looking.y -= deg2rad(event.relative.x * sensitivity)
+		looking.x -= deg2rad(event.relative.y * sensitivity)
 		looking.x = clamp(looking.x, -PI/3, PI/3)
 		viewport.rotation.y = looking.y
 		viewport.rotation.x = looking.x
@@ -43,14 +43,23 @@ func _physics_process(delta):
 	var forward = Input.is_action_pressed("player_forward")
 	var backward = Input.is_action_pressed("player_backward")
 	var left = Input.is_action_pressed("player_left")
+	var right = Input.is_action_pressed("player_right")
 	var jump = Input.is_action_just_pressed("player_jump")
 	
 	if is_on_floor() and jump:
 		velocity.y = jump_speed
+		
+	var walkvector = Vector2()
+	walkvector.x += velocity.x
+	walkvector.y -= velocity.z
 	if forward:
-		var walkvector = Vector2(walk_accel * delta, 0).rotated(viewport.rotation.y + PI/2)
-		walkvector.x += velocity.x
-		walkvector.y -= velocity.z
-		walkvector = walkvector.clamped(max_walk_speed)
-		velocity = Vector3(walkvector.x, velocity.y, -walkvector.y)
+		walkvector += Vector2(walk_accel * delta, 0).rotated(viewport.rotation.y + PI/2)
+	if backward:
+		walkvector += Vector2(-walk_accel * delta, 0).rotated(viewport.rotation.y + PI/2)
+	if left:
+		walkvector += Vector2(0, walk_accel * delta).rotated(viewport.rotation.y + PI/2)
+	if right:
+		walkvector += Vector2(0, -walk_accel * delta).rotated(viewport.rotation.y + PI/2)
+	walkvector = walkvector.clamped(max_walk_speed)
+	velocity = Vector3(walkvector.x, velocity.y, -walkvector.y)
 	velocity = move_and_slide(velocity, Vector3(0, 1, 0))
